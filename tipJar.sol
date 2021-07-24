c462e32a7db940bdcec87f6a2be2a296595bcab6
hashedMae <@hashedMae>
2021-07-23 22:59:48 -0700

    functions now inside contract, added so array empties at end of night

--- tipJar.sol
+++ tipJar.sol
@@ -13,29 +13,32 @@ contract tipJar {
 * this will be used to pay out tips for who's on shift
 */
   constructor() {
-    address _employees[];
+    address[] _employees;
   }
-}
 
-function tip() public payable {
-  require(msg.value > 0);
-}
+  function tip() public payable {
+    require(msg.value > 0);
+  }
 
-function addToPool(address _employeeAddress) public {
-  _employees.push(_employeeAddress);
-}
+  function addToPool(address _employeeAddress) public {
+    _employees.push(_employeeAddress);
+  }
 
-function _splitItUp() internal memory returns(uint){
-  uint _onShift = _employees.length();
-  return _onShift;
-}
+  function _splitItUp() internal memory returns(uint){
+    uint _onShift = _employees.length();
+    return _onShift;
+  }
 
-function payOut() public {
-  uint share = balanceOf(this) % _splitItUp();
-  i = 0
-  for(i < _employees.length) {
-    transfer(_employees[i], share);
-    i++;
+  function payOut() public {
+    uint share = balanceOf(this) % _splitItUp();
+    i = 0
+    for(i < _employees.length()) {
+      transfer(_employees[i], share);
+      i++;
+    }
+    for(i < _employees.length()) {
+      _employees.shift();
+    }
   }
 
 }
